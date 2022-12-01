e7125f7fcaff79f9829a638e2610768f5d90bb4c
W1ckedW1tch <112077394+WitchDevelops@users.noreply.github.com>
2022-12-01 20:16:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </div>
+    
+    </>
+    
+  )
+    
+}
+
+export default App;
